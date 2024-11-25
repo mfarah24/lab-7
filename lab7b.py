6f27ab8ec83910aa9a8e4ce7b151dcb67007ac3f
Mohamed Farah <145224968+mfarah24@users.noreply.github.com>
2024-11-24 19:04:09 -0500

    Add files via upload

--- lab7b.py
+++ lab7b.py
@@ -36,16 +36,21 @@ def sum_times(t1, t2):
 def change_time(time, seconds):
     time.second += seconds
     
-    # Adjust seconds if they are negative
     while time.second < 0:
         time.second += 60
         time.minute -= 1
     
-    # Adjust minutes if they are negative
+    while time.second >= 60:
+        time.second -= 60
+        time.minute += 1
+
     while time.minute < 0:
         time.minute += 60
         time.hour -= 1
 
+    while time.minute >= 60:
+        time.minute -= 60
+        time.hour += 1
     return None
 
 def valid_time(t):
@@ -56,5 +61,3 @@ def valid_time(t):
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
         return False
     return True
-
-
--- lab7c.py
+++ lab7c.py
@@ -16,37 +16,26 @@ def format_time(t):
 
 def sum_times(t1, t2):
     """Add two time objects and return the sum."""
-    sum = Time(0, 0, 0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    sum.second = t1.second + t2.second
-
-    if sum.second >= 60:
-        more_minutes = sum.second // 60
-        sum.second = sum.second % 60
-        sum.minute += more_minutes
-
-    if sum.minute >= 60:
-        extra_hours = sum.minute // 60
-        sum.minute = sum.minute % 60
-        sum.hour += extra_hours
-
-    return sum
+    total_seconds = time_to_sec(t1) + time_to_sec(t2)
+    return sec_to_time(total_seconds)
 
 def change_time(time, seconds):
-    time.second += seconds
-    
-    # Adjust seconds if they are negative
-    while time.second < 0:
-        time.second += 60
-        time.minute -= 1
-    
-    # Adjust minutes if they are negative
-    while time.minute < 0:
-        time.minute += 60
-        time.hour -= 1
+    total_seconds = time_to_sec(time) + seconds
+    new_time = sec_to_time(total_seconds)
+    time.hour, time.minute, time.second = new_time.hour, new_time.minute, new_time.second
 
-    return None
+def time_to_sec(time):
+    '''Convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):   
+    '''Convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
 
 def valid_time(t):
     """Check for the validity of the time object attributes:
@@ -56,5 +45,3 @@ def valid_time(t):
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
         return False
     return True
-
-
